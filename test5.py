4c5921f04c1cfb0efc5ef041c5a2b9365335ae96
张杰 <110650497+zj-py-123@users.noreply.github.com>
2022-08-26 10:31:04 +0800

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,6 +1,6 @@
 import easygui
 import os
-choice=easygui.buttonbox("想要获取该作品的技术吗？","问答",("想！","不想……"))
+choice=easygui.buttonbox("想要源代码吗？","问答",("想！","不想……"))
 if choice=="想！":
     easygui.msgbox("https://github.com/zj-py-123/games/tree/main","问答")
 elif choice=="不想……":
