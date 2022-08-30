549de40a696dc505f521e37b6e6f31540e77b609
张杰 <110650497+zj-py-123@users.noreply.github.com>
2022-08-30 09:28:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 import platform
 import easygui
 import os
-import random
 from pygame.locals import *
 pygame.init()
 canvas=pygame.display.set_mode((1000,600))
@@ -19,27 +18,21 @@ else:
 text=font.render("史上最坑人的游戏",True,(0,0,0))
 start=font1.render("开始游戏",True,(0,0,0))
 noMouseTip=font2.render("没鼠标的先把鼠标买了再说！",True,(0,0,0))
-button=random.randint(2,3)
 def handleEvent():
     for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
             exit()
-        elif event.type==MOUSEBUTTONDOWN and\
-             event.button==button:
+        elif event.type==MOUSEBUTTONDOWN and event.button==3:
             mouseX,mouseY=pygame.mouse.get_pos()
-            if 400<=mouseX<=600 and \
-                400<=mouseY<=450:
-                easygui.msgbox("你来了，别忘记再玩一遍",
-                               "第一关")
+            if 400<=mouseX<=600 and 400<=mouseY<=450:
                 pygame.quit()
                 return "EXIT"
-        elif event.type==MOUSEBUTTONDOWN and \
-            (event.button==1 or event.button==2):
+        elif event.type==MOUSEBUTTONDOWN and event.button==1:
             easygui.msgbox("这不是开始按钮")
 canvas.blit(text,(100,100))
 canvas.blit(start,(400,400))
-canvas.blit(noMouseTip,(320,580))
+canvas.blit(noMouseTip,(370,580))
 pygame.display.update()
 while True:
     event=handleEvent()
