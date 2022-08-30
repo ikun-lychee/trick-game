ac254527c4be58dec76db2efbaebf8062dc95938
张杰 <110650497+zj-py-123@users.noreply.github.com>
2022-08-30 09:31:08 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -12,27 +12,20 @@ if platform.system()=="Windows":
     font=pygame.font.SysFont("SimHei",50)
 else:
     font=pygame.font.SysFont("Arial Unicode MS",50)
-text=font.render("帮我关掉",True,(0,0,0))
-button=font.render("窗口",True,(0,0,0))
+text=font.render("帮我关掉窗口",True,(0,0,0))
 def handleEvent():
     for event in pygame.event.get():
         if event.type==QUIT:
-            pygame.quit()
-            exit()
+            easygui.msgbox("hhh你傻")
         elif event.type==MOUSEBUTTONDOWN:
             mouseX,mouseY=pygame.mouse.get_pos()
-            if 550<=mouseX<=650 and 275<=mouseY<=325:
+            if 450<=mouseX<=550 and 275<=mouseY<=325:
                 pygame.quit()
                 return "QUIT"
 canvas.blit(text,(350,275))
-canvas.blit(button,(550,275))
 pygame.display.update()
 while True:
     event=handleEvent()
     if event=="QUIT":
         break
-while True:
-    turnback=easygui.msgbox("终于关掉了\n最不像答案的答案就是答案","第二关",ok_button="别点我")
-    if turnback:
-        os.system("python test2.py")
-        break
+os.system("python test2.py")
