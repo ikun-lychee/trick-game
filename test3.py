db0bb73c04cbe4a85a2b34800dd12cbd0592465b
张杰 <110650497+zj-py-123@users.noreply.github.com>
2022-08-30 09:35:14 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -17,13 +17,19 @@ else:
     bf=pygame.font.SysFont("Arial Unicode MS",20)
 title=font.render("帮我把算式调成成立的",True,(0,0,0))
 calculation=font1.render("1+1=1",True,(0,0,0))
+blank=pygame.image.load("blank.png")
 def handleEvent():
     for event in pygame.event.get():
         if event.type==QUIT:
-            pygame.quit()
-            exit()
+            easygui.msgbox("别跑！")
         elif event.type==MOUSEWHEEL:
+            canvas.blit(blank,(0,0))
+            canvas.blit(title,(0,0))
+            calculation=font1.render("1x1=1",True,(0,0,0))
+            canvas.blit(calculation,(450,275))
+            pygame.display.update()
             easygui.msgbox("惊不惊喜，意不意外？")
+            pygame.quit()
             return "QUIT"
 canvas.blit(title,(0,0))
 canvas.blit(calculation,(450,275))
